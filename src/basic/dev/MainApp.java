1b0168026803dbd492022c3adf57176c919e542e
DUNG <dungnguyen9903@gmail.com>
2021-10-25 15:16:28 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,33 @@
+package basic.dev;
+/**
+ * Ghi chú( comment nhiều dòng)
+ * @author NguyenPhiDung
+ * 1. Tên lớp trùng tên file
+ * 2. Tên lớp chữ cái đầu viết hoa. Nếu từ ghép thì những chữ cái đầu viết hoa
+ * 3. Tên lớp đặt phải có ý nghĩa. Ví dụ: class Person: lớp mô tả con người
+ * -Interface: Phải có tiền tố I. Ví dụ: IListener
+ * -Abstract: Phải cótiền tố Abstract. Vídụ: AbstractShape
+ */
+
+public class MainApp {
+	//Comment 1 dòng cách 1. Phím nóng Ctr+/
+	/*Comment 1 dòng cách 2*/
+	public static void main(String[] args ) {
+		System.out.println("Hello World");
+		/**
+		 * Cách đặt tên biến:
+		 *  1. Không chứa ký tự đặc biệt !@#$%^&*()-+=
+		 *  2. Không chứa khoảng trắng
+		 *  3. Viết thường
+		 *  4. Không được bắt đầu bằng số
+		 */
+		int number;
+		String nameSchool;
+	}
+	/*
+	 * tên phương thức tuân theo quy tắc camelCase
+	 */
+	public void show() {
+
+}
+}
